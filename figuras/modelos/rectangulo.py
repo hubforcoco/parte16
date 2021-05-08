61f28087d97e0c249eab64be1bb804d1ee70ce46
hubforcoco <cocotareas@gmail.com>
2021-05-08 16:54:49 -0500

    hasta el video 365

---
+++ figuras/modelos/rectangulo.py
@@ -0,0 +1,17 @@
+from .figura import Figura
+
+class Rectangulo(Figura):
+    def __init__(self, color_fondo, color_borde, largo, ancho):
+        super().__init__(color_fondo, color_borde):
+
+        self.ancho = ancho
+        self.alto = alto
+
+
+    def dibujar(self):
+        print(f'Se está dibujando el rectángulo con medidas  ancho {self.ancho} y alto {self.alto}')
+
+    def area(self):
+        resultado = self.largo * self.ancho
+        return resultado
+
